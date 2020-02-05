fc88f45fcde2180ed6b347bcb6f69204a7602574
Saracen <SaracenOne@gmail.com>
2020-02-05 21:24:10 +0000

    Add typing

--- error_manager_plugin.gd
+++ error_manager_plugin.gd
@@ -1,11 +1,11 @@
 tool
 extends EditorPlugin
 
-func get_name(): 
+func get_name() -> String:
 	return "ErrorManager"
 
-func _enter_tree():
+func _enter_tree() -> void:
 	add_autoload_singleton("ErrorManager", "res://addons/error_manager/error_manager.gd")
 	
-func _exit_tree():
+func _exit_tree() -> void:
 	remove_autoload_singleton("ErrorManager")
