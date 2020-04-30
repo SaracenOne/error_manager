a4fc45a2803c2b0980b4516384aaeb199be9e287
Saracen <SaracenOne@gmail.com>
2020-04-30 12:43:57 +0100

    Add info and stack trace to errors

--- error_manager.gd
+++ error_manager.gd
@@ -5,9 +5,11 @@ func printl(p_line_string):
 	print(p_line_string)
 	
 func error(p_error_string):
-	printerr(p_error_string)
+	print_stack()
+	printerr("ERROR: " + p_error_string)
 	
 func fatal_error(p_error_string):
-	printerr(p_error_string)
+	print_stack()
+	printerr("FATAL ERROR: " + p_error_string)
 	if not Engine.is_editor_hint():
 		get_tree().quit() #default behavior
