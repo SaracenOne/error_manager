529a229af53b843d365d909bc64e8d3bbe0424f4
Saracen <SaracenOne@gmail.com>
2020-07-20 10:58:19 +0100

    Converted string concatenations to formats, remove explicit boolean conditionals.

--- error_manager.gd
+++ error_manager.gd
@@ -6,10 +6,10 @@ func printl(p_line_string):
 	
 func error(p_error_string):
 	print_stack()
-	printerr("ERROR: " + p_error_string)
+	printerr("ERROR: %s" % p_error_string)
 	
 func fatal_error(p_error_string):
 	print_stack()
-	printerr("FATAL ERROR: " + p_error_string)
+	printerr("FATAL ERROR: %s" % p_error_string)
 	if not Engine.is_editor_hint():
 		get_tree().quit() #default behavior
