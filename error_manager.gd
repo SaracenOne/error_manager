78c5f0c9242a76284d79e24162a6845ee84b77db
Saracen <SaracenOne@gmail.com>
2020-08-04 01:06:14 +0100

    Run GDFormat

--- error_manager.gd
+++ error_manager.gd
@@ -1,15 +1,18 @@
 extends Node
 tool
 
+
 func printl(p_line_string):
 	print(p_line_string)
-	
+
+
 func error(p_error_string):
 	print_stack()
 	printerr("ERROR: %s" % p_error_string)
-	
+
+
 func fatal_error(p_error_string):
 	print_stack()
 	printerr("FATAL ERROR: %s" % p_error_string)
 	if not Engine.is_editor_hint():
-		get_tree().quit() #default behavior
+		get_tree().quit()  #default behavior
--- error_manager_plugin.gd
+++ error_manager_plugin.gd
@@ -1,11 +1,14 @@
 tool
 extends EditorPlugin
 
+
 func get_name() -> String:
 	return "ErrorManager"
 
+
 func _enter_tree() -> void:
 	add_autoload_singleton("ErrorManager", "res://addons/error_manager/error_manager.gd")
-	
+
+
 func _exit_tree() -> void:
 	remove_autoload_singleton("ErrorManager")
